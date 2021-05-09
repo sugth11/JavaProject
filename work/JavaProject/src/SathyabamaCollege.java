bb56f14b74021fbb4de4421db12c54717b9f28d5
Sujith <sujithjoseph.ir@gmail.com>
2021-05-09 15:52:48 +0530

    my java prectise

---
+++ work/JavaProject/src/SathyabamaCollege.java
@@ -0,0 +1,10 @@
+public class SathyabamaCollege extends University{
+    @Override
+    void location(){
+        System.out.println("Shollinganallur");
+    }
+    void departments(){
+        System.out.println("10 departments");
+    }
+
+}
---
+++ work/JavaProject/src/University.java
@@ -0,0 +1,11 @@
+public class University {
+    void location(){
+        System.out.println("chennai");
+    }
+    void timings(){
+        System.out.println("morning 9 to evening 5");
+    }
+    void buses(){
+        System.out.println("200 number of buses");
+    }
+}
